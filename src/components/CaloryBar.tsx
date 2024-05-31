22f514c583618ea92205886f32a7863323585995
rosevilleage <chanhee.frontend@gmail.com>
2024-05-31 18:34:38 +0900

    feat : CaloryBar 컴포넌트 생성

---
+++ src/components/CaloryBar.tsx
@@ -0,0 +1,60 @@
+import {StyleSheet, Text, View} from 'react-native';
+interface CaloryBarProps {
+  type: 'amr' | 'total';
+  calory: number;
+  percent: number;
+}
+const CaloryBar = ({type, calory, percent}: CaloryBarProps) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        {type === 'amr' ? '일일 권장 칼로리' : '현재까지 섭취 칼로리'}
+      </Text>
+      <View style={styles.rowBox}>
+        <View
+          style={[
+            {width: `${percent}%`},
+            styles.valueBar,
+            type === 'amr' ? styles.totalBar : styles.currentBar,
+          ]}>
+          {percent > 30 && <Text style={styles.text}>{calory}</Text>}
+        </View>
+        {percent <= 30 && <Text style={styles.text}>{calory}</Text>}
+      </View>
+    </View>
+  );
+};
+
+export default CaloryBar;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 5,
+  },
+  rowBox: {
+    width: '100%',
+    flexDirection: 'row',
+  },
+  valueBar: {
+    paddingVertical: 10,
+    borderRadius: 13,
+  },
+  totalBar: {
+    backgroundColor: '#F6E54D',
+  },
+  currentBar: {
+    backgroundColor: '#FFE3C9',
+  },
+  text: {
+    color: '#000',
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  title: {
+    fontSize: 16,
+    color: '#000',
+    marginBottom: 3,
+  },
+});
