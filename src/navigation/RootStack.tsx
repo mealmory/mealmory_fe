103b98ec635d002425f822cae6e2d2278551e545
rosevilleage <chanhee.frontend@gmail.com>
2024-06-05 23:39:29 +0900

    feat : 화면 전환 애니메이션 설정

--- src/navigation/RootStack.tsx
+++ src/navigation/RootStack.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 import MainTaps from './MainTabs';
+import MealPlanDetailByTime from '../screens/MealPlanDetailByTime';
+import {RootStackParamList} from './navigation';
 
-const RootStack = createStackNavigator();
+const RootStack = createStackNavigator<RootStackParamList>();
 
 export default function RootNavigation() {
   return (
-    <RootStack.Navigator initialRouteName="MainTap">
+    <RootStack.Navigator
+      initialRouteName="MainTab"
+      screenOptions={{
+        headerTitleAlign: 'center',
+        gestureEnabled: true,
+        cardOverlayEnabled: true,
+        ...TransitionPresets.SlideFromRightIOS,
+      }}>
       <RootStack.Screen
         name="MainTab"
         component={MainTaps}
         options={{headerShown: false}}
+      />
+      {/* common screens */}
+      <RootStack.Screen
+        name="MealPlanByTime"
+        component={MealPlanDetailByTime}
+        options={({route}) => ({title: route.params.time})}
       />
     </RootStack.Navigator>
   );
