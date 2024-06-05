eda82eb0144497322c192796885b6715d3c483f7
rosevilleage <chanhee.frontend@gmail.com>
2024-06-05 23:39:51 +0900

    style : svg 경로 수정

--- src/util/svgUtil.tsx
+++ src/util/svgUtil.tsx
@@ -1,11 +1,11 @@
-import HomeLineIcon from './../assets/tabIcon/homeLine.svg';
-import HomeFillIcon from './../assets/tabIcon/homeFill.svg';
-import GraphLineIcon from './../assets/tabIcon/graphLine.svg';
-import GraphFillIcon from './../assets/tabIcon/graphFill.svg';
-import MyLineIcon from './../assets/tabIcon/myLine.svg';
-import MyFillIcon from './../assets/tabIcon/myFill.svg';
-import MoreLineIcon from './../assets/tabIcon/menuLine.svg';
-import MoreFillIcon from './../assets/tabIcon/menuFill.svg';
+import HomeLineIcon from './../assets/tabIcon/homeIconLine.svg';
+import HomeFillIcon from './../assets/tabIcon/homeIconFill.svg';
+import GraphLineIcon from './../assets/tabIcon/graphIconLine.svg';
+import GraphFillIcon from './../assets/tabIcon/graphIconFill.svg';
+import MyLineIcon from './../assets/tabIcon/myIconLine.svg';
+import MyFillIcon from './../assets/tabIcon/myIconFill.svg';
+import MoreLineIcon from './../assets/tabIcon/menuIconLine.svg';
+import MoreFillIcon from './../assets/tabIcon/menuIconFill.svg';
 
 export function genTabIcon({
   focused,
