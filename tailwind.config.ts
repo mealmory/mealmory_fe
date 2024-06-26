94debc37d399801881ada1a38d672048088c7f8c
rosevilleage <chanhee.frontend@gmail.com>
2024-06-26 22:23:16 +0900

    custom color 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,14 @@ const config: Config = {
       boxShadow: {
         border: "0 0 3px 0 rgba(0, 0, 0, 0.3)",
       },
+      backgroundColor: {
+        cusyellow: "#F6E54D",
+        cusbanana: "#FFFEF7",
+        cuspink: "#FFE3C9",
+        cusorange: "#FFB800",
+        cusgray: "#E6E6E6",
+        dcusgray: "#5A5A5A",
+      },
     },
   },
   plugins: [],
