f90f075373a02a4043c575311fc79380d9f1b30c
rosevilleage <chanhee.frontend@gmail.com>
2024-06-26 17:15:53 +0900

    box shadow border용 생성

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,11 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        border: "0 0 3px 0 rgba(0, 0, 0, 0.3)",
+      },
+    },
   },
   plugins: [],
 };
