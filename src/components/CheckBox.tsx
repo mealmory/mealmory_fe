3b561219ff3e83e4d4820da660e5000224a5a9e9
rosevilleage <chanhee.frontend@gmail.com>
2024-06-26 22:23:56 +0900

    checkbox component 생성

---
+++ src/components/CheckBox.tsx
@@ -0,0 +1,34 @@
+import { useId } from "react";
+import { BsCheck } from "@react-icons/all-files/bs/BsCheck";
+interface CheckBoxProps {
+  name: string;
+  handleClick: () => void;
+  checked: boolean;
+}
+const CheckBox = ({ name, handleClick, checked }: CheckBoxProps) => {
+  const uuid = useId();
+  return (
+    <label
+      htmlFor={"checkbox-" + uuid}
+      className="flex items-center gap-1 w-max"
+    >
+      <input
+        id={"checkbox-" + uuid}
+        type="checkbox"
+        onChange={handleClick}
+        className=" hidden"
+        checked={checked}
+      />
+      <div
+        className={`border border-gray-700 rounded-md w-5 h-5 flex items-center justify-center ${
+          checked ? "bg-[#FFFBD3] text-[#FFB800]" : ""
+        }`}
+      >
+        {checked && <BsCheck size={18} />}
+      </div>
+      <span>{name}</span>
+    </label>
+  );
+};
+
+export default CheckBox;
