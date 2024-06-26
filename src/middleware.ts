dc774df7ea7e1fb18a30f0c34a334b0cc6995448
rosevilleage <chanhee.frontend@gmail.com>
2024-06-26 17:15:20 +0900

    로그인 여부 redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,19 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
   const reqHeaders = new Headers(req.headers);
-  reqHeaders.set("x-pathname", req.nextUrl.pathname);
-  return NextResponse.next({
-    request: {
-      headers: reqHeaders,
-    },
-  });
+  const { pathname } = req.nextUrl;
+  reqHeaders.set("x-pathname", pathname);
+  if (pathname === "/") {
+    if (req.cookies.get("mact")) {
+      return NextResponse.redirect(new URL("/home", req.url));
+    } else {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  } else {
+    return NextResponse.next({
+      request: {
+        headers: reqHeaders,
+      },
+    });
+  }
 }
