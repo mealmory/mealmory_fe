06b1587edfbd4bf4378f902b21139544637719f6
rosevilleage <chanhee.frontend@gmail.com>
2024-06-28 22:23:13 +0900

    user profile 컴포넌트 생성

---
+++ src/components/userInfo/UserProfileInfo.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+interface UserProfileInfoProps {
+  profileData: {
+    image: string;
+    nickname: string;
+    email: string;
+  };
+}
+
+const UserProfileInfo = async ({ profileData }: UserProfileInfoProps) => {
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <Image
+        src={profileData.image}
+        width={107}
+        height={107}
+        className=" rounded-full"
+        alt="나의 카카오 프로필 사진"
+        priority
+      />
+      <div className=" text-gray-500 text-center dark:text-white">
+        <p>{profileData.email}</p>
+        <p>{profileData.nickname}</p>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfileInfo;
