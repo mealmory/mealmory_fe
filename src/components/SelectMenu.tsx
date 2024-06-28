13f3136416caf17228ad17fbcbee8ba76179ecef
rosevilleage <chanhee.frontend@gmail.com>
2024-06-29 00:36:47 +0900

    props 변경

--- src/components/SelectMenu.tsx
+++ src/components/SelectMenu.tsx
@@ -5,40 +5,32 @@ import { Dispatch, SetStateAction } from "react";
 
 const Select = dynamic(() => import("react-select"), {
   ssr: false,
-  loading: () => <div className="rounded-xl w-full h-[13vw] border" />,
+  loading: () => <div className="rounded-xl w-full pt-[13.3%] border" />,
 });
 
 interface SelectMenuProps {
   label: string;
-  name: string;
   options: Array<{ value: number; label: string }>;
   disabled?: boolean;
-  setSelectedOption: Dispatch<
-    SetStateAction<{
-      birth: number;
-      actLevel: number;
-    }>
-  >;
+  value: number;
+  handleChange: (e: unknown) => void;
 }
 
 const SelectMenu = ({
   options,
-  setSelectedOption,
   label,
-  name,
+  value,
   disabled,
+  handleChange,
 }: SelectMenuProps) => {
   return (
     <div>
       <p>{label}</p>
       <Select
         options={options}
-        defaultValue={options[0]}
+        value={options.filter((option) => option.value === value)}
         isDisabled={disabled}
-        onChange={(e) => {
-          const event = e as { value: number; label: string };
-          setSelectedOption((prev) => ({ ...prev, [name]: event.value }));
-        }}
+        onChange={handleChange}
         styles={{
           control: (styles) => ({
             ...styles,
