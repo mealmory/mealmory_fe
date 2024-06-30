7608b48c6f57b04a783896a81f02da828daaccad
rosevilleage <chanhee.frontend@gmail.com>
2024-06-30 23:54:01 +0900

    main constants

---
+++ src/constants/mainConstants.ts
@@ -0,0 +1,11 @@
+export const MAIN_LABELS = {
+  caloryData: {
+    amr: "일일 권장 칼로리(활동 대사량)",
+    totalCalory: "현재까지 섭취 칼로리",
+  },
+  avg: {
+    bmi: "비만도",
+    bmr: "기초대사량",
+    weight: "체중",
+  },
+};
