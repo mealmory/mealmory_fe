a8be7b866ff8fbbed06448c1fa5004a245eb30be
rosevilleage <chanhee.frontend@gmail.com>
2024-07-02 00:22:03 +0900

    nav 상수 파일 추가

---
+++ src/constants/navConstants.ts
@@ -0,0 +1,31 @@
+import { BsHouse } from "@react-icons/all-files/bs/BsHouse";
+import { BsHouseFill } from "@react-icons/all-files/bs/BsHouseFill";
+import { BsBarChart } from "@react-icons/all-files/bs/BsBarChart";
+import { BsBarChartFill } from "@react-icons/all-files/bs/BsBarChartFill";
+import { BsPerson } from "@react-icons/all-files/bs/BsPerson";
+import { BsPersonFill } from "@react-icons/all-files/bs/BsPersonFill";
+import { BsGrid } from "@react-icons/all-files/bs/BsGrid";
+import { BsGridFill } from "@react-icons/all-files/bs/BsGridFill";
+
+export const LINK_ITEMS = [
+  {
+    title: "메인",
+    link: "/home",
+    icon: { normal: BsHouse, matched: BsHouseFill },
+  },
+  {
+    title: "통계",
+    link: "/graph",
+    icon: { normal: BsBarChart, matched: BsBarChartFill },
+  },
+  {
+    title: "프로필",
+    link: "/profile",
+    icon: { normal: BsPerson, matched: BsPersonFill },
+  },
+  {
+    title: "더보기",
+    link: "/more",
+    icon: { normal: BsGrid, matched: BsGridFill },
+  },
+];
