a0e9e045869582457c67adac8fb5ca406679eaa7
rosevilleage <chanhee.frontend@gmail.com>
2024-07-03 03:14:58 +0900

    bmi 상태 계산 유틸함수

---
+++ src/utils/checkBmi.ts
@@ -0,0 +1,19 @@
+export function checkBmi(bmi: number) {
+  // < 18.5 저체중 파
+  // < 23 정상 초
+  // < 25 과체중 주
+  // < 25 비만 빨
+  if (0 < bmi && bmi < 18.5) {
+    return { text: "저체중", status: "less" };
+  }
+  if (18.5 <= bmi && bmi < 23) {
+    return { text: "정상", status: "safe" };
+  }
+  if (23 <= bmi && bmi < 25) {
+    return { text: "과체중", status: "warning" };
+  }
+  if (25 <= bmi) {
+    return { text: "비만", status: "danger" };
+  }
+  return { text: "측정불가", status: "none" };
+}
