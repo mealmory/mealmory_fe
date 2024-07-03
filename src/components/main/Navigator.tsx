4f36f038dee39e9662c4af1b100339a7c735783e
rosevilleage <chanhee.frontend@gmail.com>
2024-07-03 20:31:53 +0900

    Navigator 역할 간소화

--- src/components/main/Navigator.tsx
+++ src/components/main/Navigator.tsx
@@ -1,61 +1,54 @@
-"use client";
-
 import { LINK_ITEMS } from "@/constants/navConstants";
 import { IconType } from "@react-icons/all-files";
-import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
-export default function Navigator() {
-  const pathname = usePathname();
-  const showFlag =
-    pathname === "/main" ||
-    pathname === "/main/graph" ||
-    pathname === "/main/profile" ||
-    pathname === "/main/more";
+interface NavigatorProps {
+  pathname: string;
+  navClass?: string;
+  middleClass?: string;
+  itemClass?: string;
+  iconSize?: number | string;
+}
+
+export default function Navigator({
+  pathname,
+  navClass,
+  middleClass,
+  itemClass,
+  iconSize,
+}: NavigatorProps) {
   return (
-    <>
-      {showFlag ? (
-        <header className="sm:h-screen h-max sm:w-40 w-full fixed top-0 left-0 bg-white dark:bg-cusdark z-10">
-          <nav className="flex sm:flex-col items-center gap-5 justify-center sm:justify-start sm:p-4 sm:shadow-border h-full">
-            <div className=" self-center flex items-center gap-1">
-              <Image
-                src={"/mealmory_logo.svg"}
-                alt="밀모리 로고"
-                width={0}
-                height={0}
-                className="w-full h-auto"
-                priority
-              />
-              <p>MealMory</p>
-            </div>
-            <div className="hidden sm:flex flex-col gap-4">
-              {LINK_ITEMS.map(({ title, link, icon }) => (
-                <NavItem
-                  key={title}
-                  title={title}
-                  link={link}
-                  active={pathname === link}
-                  icon={icon}
-                />
-              ))}
-            </div>
-          </nav>
-        </header>
-      ) : null}
-    </>
+    <nav className={navClass}>
+      <div className={middleClass}>
+        {LINK_ITEMS.map(({ title, link, icon }) => (
+          <NavItem
+            key={title}
+            title={title}
+            link={link}
+            active={pathname === link}
+            className={itemClass}
+            icon={icon}
+            iconSize={iconSize}
+          />
+        ))}
+      </div>
+    </nav>
   );
 }
 
-const NavItem = ({
+export const NavItem = ({
   title,
   link,
   active,
   icon,
+  className,
+  iconSize,
 }: {
   title: string;
   link: string;
   active: boolean;
+  className?: string;
+  iconSize?: number | string;
   icon: {
     normal: IconType;
     matched: IconType;
@@ -64,11 +57,11 @@ const NavItem = ({
   const { matched: ActiveIcon, normal: NormalIcon } = icon;
   return (
     <Link
-      className={"flex gap-1 items-center " + (active ? "font-bold" : "")}
+      className={(className ?? "") + "" + (active ? " font-bold" : "")}
       href={link}
     >
-      {active ? <ActiveIcon /> : <NormalIcon />}
-      {title}
+      {active ? <ActiveIcon size={iconSize} /> : <NormalIcon size={iconSize} />}
+      <p>{title}</p>
     </Link>
   );
 };
