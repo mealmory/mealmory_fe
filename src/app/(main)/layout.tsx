b1e1bcbc29a95fe6d0247009501927425a11d0f1
rosevilleage <chanhee.frontend@gmail.com>
2024-07-04 00:32:03 +0900

    padding bottom 수정

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -7,7 +7,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="w-full min-h-screen relative sm:pl-40 sm:pt-0 sm:pb-0 pt-[45px] pb-[52px]">
+    <div className="w-full min-h-screen relative sm:pl-40 sm:pt-0 sm:pb-0 pt-[45px] pb-[55px]">
       <Header />
       <main className="h-full w-full text-xl sm:text-base">{children}</main>
       <Footer />
