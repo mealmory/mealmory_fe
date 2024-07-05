2b5b4f26994fef78f029f507f394342a21a622ee
rosevilleage <chanhee.frontend@gmail.com>
2024-07-06 01:00:56 +0900

    date 추가

--- src/app/api/dummy/notice/search/route.ts
+++ src/app/api/dummy/notice/search/route.ts
@@ -5,6 +5,7 @@ const notifications = Array.from({ length: 10 }, (_, i) => ({
   title: `${10 - (i + 1)}월 공지사항`,
   description:
     "선배 탕후루 사주세요. 혹시 마라탕도? 그럼 제가 선배맘에 탕탕 후루후루",
+  date: "2024-04-12",
 }));
 
 export function GET() {
