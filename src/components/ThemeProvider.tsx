d4518cb2be0bb55bd08582c5fd4be070bb75dfb3
rosevilleage <chanhee.frontend@gmail.com>
2024-07-05 18:18:40 +0900

    theme provider 컴포넌트 생성

---
+++ src/components/ThemeProvider.tsx
@@ -0,0 +1,12 @@
+"use clinet";
+
+import { ThemeProvider as NextThemeProvider } from "next-themes";
+import { ThemeProviderProps } from "next-themes/dist/types";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextThemeProvider attribute="class" defaultTheme="system" {...props}>
+      {children}
+    </NextThemeProvider>
+  );
+}
