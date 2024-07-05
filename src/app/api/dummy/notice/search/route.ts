ea82e6f065bb8004e6526e3fd65e561a98e3e845
rosevilleage <chanhee.frontend@gmail.com>
2024-07-05 22:32:14 +0900

    dummy api 생성

---
+++ src/app/api/dummy/notice/search/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+const notifications = Array.from({ length: 10 }, (_, i) => ({
+  id: i,
+  title: `${10 - (i + 1)}월 공지사항`,
+  description:
+    "선배 탕후루 사주세요. 혹시 마라탕도? 그럼 제가 선배맘에 탕탕 후루후루",
+}));
+
+export function GET() {
+  return NextResponse.json(notifications);
+}
