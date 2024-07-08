eaea947d53e0c9b4c6f579605ec5599cab068655
rosevilleage <chanhee.frontend@gmail.com>
2024-07-08 18:31:42 +0900

    mealplan dummy api 생성

---
+++ src/app/api/dummy/meal/info/route.ts
@@ -0,0 +1,19 @@
+import { genTempDto } from "@/utils/genTempDTO";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
+const menuList = Array.from({ length: 4 }, () => ({
+  menu: "밥",
+  calory: 12345,
+  weight: 214,
+  unit: 1,
+}));
+export function GET(req: NextApiRequest) {
+  return NextResponse.json(
+    genTempDto({
+      type: "아침",
+      date: "2024-04-12",
+      menuList: menuList,
+      total: 1234,
+    })
+  );
+}
---
+++ src/app/api/dummy/meal/search/route.ts
@@ -0,0 +1,15 @@
+import { genTempDto } from "@/utils/genTempDTO";
+import { NextResponse } from "next/server";
+
+const mealPlan = [
+  { id: 1, type: "아침", time: "2024-05-15 08:00:00", total: 123456 },
+  { id: 2, type: "간식", time: "2024-05-15 10:00:00", total: 14515 },
+  { id: 4, type: "점심", time: "2024-05-15 12:00:00", total: 754824 },
+  { id: 3, type: "간식", time: "2024-05-15 15:00:00", total: 15854 },
+  { id: 10, type: "저녁", time: "2024-05-15 18:00:00", total: 4185445 },
+  { id: 13, type: "야식", time: "2024-05-15 21:00:00", total: 1548 },
+];
+
+export function GET() {
+  return NextResponse.json(genTempDto(mealPlan));
+}
