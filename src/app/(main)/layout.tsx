c05ca76fe68106613c8d7f502c1b80244dd02950
rosevilleage <chanhee.frontend@gmail.com>
2024-07-12 17:39:40 +0900

    main 페이지 text size 수정

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,7 +9,7 @@ export default function MainLayout({
   return (
     <div className="w-full min-h-screen relative sm:pl-40 sm:pt-0 sm:pb-0 pt-[45px] pb-[55px]">
       <Header />
-      <main className="h-full w-full text-xl sm:text-base">{children}</main>
+      <main className="h-full w-full">{children}</main>
       <Footer />
     </div>
   );
