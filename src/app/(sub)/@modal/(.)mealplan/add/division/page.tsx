64ec10ba29559c8804c918df6173eea05c65079b
rosevilleage <chanhee.frontend@gmail.com>
2024-07-19 00:24:23 +0900

    메뉴 카테고리 선택 modal, page 생성

---
+++ src/app/(sub)/@modal/(.)mealplan/add/division/page.tsx
@@ -0,0 +1,10 @@
+import DivisionSelectComponent from "@/components/modal/division/DivistionSelectComponent";
+import Modal from "@/components/modal/Modal";
+
+export default function DivisionModalPage() {
+  return (
+    <Modal>
+      <DivisionSelectComponent />
+    </Modal>
+  );
+}
--- src/app/(sub)/mealplan/add/division/page.tsx
+++ src/app/(sub)/mealplan/add/division/page.tsx
@@ -1,3 +1,11 @@
+import DivisionSelectComponent from "@/components/modal/division/DivistionSelectComponent";
+
 export default function CategorySelectPage() {
-  return <div>modal</div>;
+  return (
+    <div className="bg-black bg-opacity-35 fixed left-0 top-0 w-screen h-screen flex justify-center items-center">
+      <div className="w-full h-full md:w-[360px] sm:h-[680px] overflow-y-scroll bg-white dark:bg-cusdark sm:rounded-2xl">
+        <DivisionSelectComponent />
+      </div>
+    </div>
+  );
 }
