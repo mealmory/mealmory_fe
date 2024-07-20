a24dab9915bc985320b6c45b8f6482cd12fe71fa
rosevilleage <chanhee.frontend@gmail.com>
2024-07-20 23:34:17 +0900

    kakao sdk 설정

---
+++ src/app/KakaoScript.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Script from "next/script";
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+  type Kakao = any;
+}
+
+const KakaoScript = () => {
+  const onLoad = () => {
+    setTimeout(() => {
+      window.Kakao.init(process.env.NEXT_PUBLIC_JS_SDK_KEY);
+    }, 500);
+  };
+
+  return (
+    <Script
+      async
+      src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
+      integrity={process.env.NEXT_PUBLIC_INTEGRITY}
+      crossOrigin="anonymous"
+      onLoad={onLoad}
+    />
+  );
+};
+
+export default KakaoScript;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Jua } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import KakaoScript from "./KakaoScript";
 
 const jua = Jua({
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({
         }
       >
         <ThemeProvider>{children}</ThemeProvider>
+        <KakaoScript />
       </body>
     </html>
   );
