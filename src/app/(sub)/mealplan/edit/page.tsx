6fe42fb91be36303f37debfa68874f0646b0f67c
rosevilleage <chanhee.frontend@gmail.com>
2024-07-22 19:20:01 +0900

    식단 추가, 수정 페이지

--- src/app/(sub)/mealplan/edit/page.tsx
+++ src/app/(sub)/mealplan/edit/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import { useState } from "react";
 import MealplanForm from "@/components/main/MealplanForm";
+import { useState } from "react";
 
-export default function MealAddPage() {
+export default function MealEditPage() {
   const [selectedType, setSelectedType] = useState(0);
   return (
     <main>
       <MealplanForm
         selectedType={selectedType}
         setSelectedType={setSelectedType}
+        edit
       />
     </main>
   );
