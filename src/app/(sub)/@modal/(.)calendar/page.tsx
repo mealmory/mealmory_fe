d4a8d59e1a4f0dba9a23c8736eb2e267197a4db1
rosevilleage <chanhee.frontend@gmail.com>
2024-07-22 19:20:52 +0900

    식단 추가, 수정 달력 페이지 생성

---
+++ src/app/(sub)/@modal/(.)calendar/page.tsx
@@ -0,0 +1,10 @@
+import DatePicker from "@/components/modal/calendar/DatePicker";
+import Modal from "@/components/modal/Modal";
+
+export default function MealPlanCalendar() {
+  return (
+    <Modal maxContent>
+      <DatePicker />
+    </Modal>
+  );
+}
---
+++ src/app/(sub)/calendar/page.tsx
@@ -0,0 +1,8 @@
+import DatePicker from "@/components/modal/calendar/DatePicker";
+import LikeModalView from "../LikeModalView";
+
+export default function DatePickPage() {
+  <LikeModalView maxContent>
+    <DatePicker />
+  </LikeModalView>;
+}
