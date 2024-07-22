9b3385b5efb6e83fdcac0c69adfe39c529fac58f
rosevilleage <chanhee.frontend@gmail.com>
2024-07-22 19:20:17 +0900

    like modal view 적용

--- src/app/(sub)/mealplan/add/division/page.tsx
+++ src/app/(sub)/mealplan/add/division/page.tsx
@@ -1,11 +1,10 @@
+import LikeModalView from "@/app/(sub)/LikeModalView";
 import DivisionSelectComponent from "@/components/modal/division/DivistionSelectComponent";
 
 export default function CategorySelectPage() {
   return (
-    <div className="bg-black bg-opacity-35 fixed left-0 top-0 w-screen h-screen flex justify-center items-center">
-      <div className="w-full h-full md:w-[360px] sm:h-[680px] overflow-y-scroll bg-white dark:bg-cusdark sm:rounded-2xl">
-        <DivisionSelectComponent />
-      </div>
-    </div>
+    <LikeModalView>
+      <DivisionSelectComponent />
+    </LikeModalView>
   );
 }
