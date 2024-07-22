ef9e3f79ba55f194271e13666f0b449074d745a1
rosevilleage <chanhee.frontend@gmail.com>
2024-07-22 19:18:39 +0900

    date picker component 생성

---
+++ src/components/modal/calendar/DatePicker.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Calendar from "@/components/main/Calendar";
+import useDate from "@/store/selectDateStore";
+
+const DatePicker = () => {
+  const router = useRouter();
+  const { selectedDate, changeDate } = useDate();
+  return (
+    <>
+      <div className="w-full flex justify-end">
+        <button onClick={() => router.back()}>X</button>
+      </div>
+      <div>
+        <Calendar endDate={selectedDate} handleDateChange={changeDate} />
+      </div>
+    </>
+  );
+};
+
+export default DatePicker;
