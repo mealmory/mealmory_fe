eb9f458d72ad2aaf223226a83ca7648b658a7702
rosevilleage <chanhee.frontend@gmail.com>
2024-07-30 00:01:31 +0900

    로그인 후 데이터 처리

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -3,25 +3,44 @@
 import { fetchServer } from "@/utils/fetchClient";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
-
+interface LoginResponse {
+  email: string;
+  nickName: string;
+  profile: 0 | string;
+  collect: number;
+  agreement: number;
+}
 export default function CallbackKakao() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const code = searchParams.get("code");
   useEffect(() => {
     if (code) {
-      fetchServer("auth/callback", {
+      fetchServer<LoginResponse>("auth/callback", {
         method: "POST",
         body: { code },
         credentials: "same-origin",
       }).then((res) => {
-        // console.log(res.body);
-        router.push("/auth/consent");
+        if (res.body.code === 0) {
+          const { email, nickName, profile, agreement, collect } =
+            res.body.data;
+          localStorage.setItem("email", email);
+          localStorage.setItem("nickName", nickName);
+          localStorage.setItem("profile", String(profile));
+
+          if (agreement === 0) {
+            router.replace("/auth/consent");
+          } else if (collect === 0) {
+            router.replace("/auth/user-info");
+          } else {
+            router.replace("/home");
+          }
+        }
       });
       // .catch((e) => {
       //   router.replace("/auth");
       // });
     }
-  }, [code]);
+  }, [code, router]);
   return <div></div>;
 }
