b67ad2177ce2edff245d73bca10f1e35a8b2888d
rosevilleage <chanhee.frontend@gmail.com>
2024-08-01 20:46:50 +0900

    try catch 제거

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -15,43 +15,38 @@ interface LoginResponse {
 
 export async function POST(req: NextRequest) {
   const { code } = await req.json();
-  try {
-    const res = await fetchClient<LoginResponse>("user/login", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${code}`,
-        "Content-Type": "application/json",
-      },
-      body: {
-        timestamp: getTimestamp(),
-      },
-    });
-    if (res.body.code === 0) {
-      const data = res.body.data;
-      const { accessToken, refreshToken, ...returnData } = data;
-      const response = NextResponse.json({
-        ...res.body,
-        data: returnData,
-      });
-      response.headers.set("Access-Control-Allow-Credentials", "true");
 
-      response.cookies.set("act", data.accessToken, {
-        // expires: new Date(Date.now() + data.expires_in),
-        secure: process.env.NODE_ENV === "production",
-        path: "/",
-      });
-      response.cookies.set("rft", data.refreshToken, {
-        // expires: new Date(Date.now() + data.refresh_token_expires_in),
-        secure: process.env.NODE_ENV === "production",
-        httpOnly: process.env.NODE_ENV === "production",
-        path: "/",
-      });
-      return response;
-    }
-    throw new Error("로그인 실패");
-  } catch (e) {
-    return NextResponse.json({
-      data: "fail",
+  const res = await fetchClient<LoginResponse>("user/login", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${code}`,
+      "Content-Type": "application/json",
+    },
+    body: {
+      timestamp: getTimestamp(),
+    },
+  });
+  if (res.body.code === 0) {
+    const data = res.body.data;
+    const { accessToken, refreshToken, ...returnData } = data;
+    const response = NextResponse.json({
+      ...res.body,
+      data: returnData,
     });
+    response.headers.set("Access-Control-Allow-Credentials", "true");
+
+    response.cookies.set("act", data.accessToken, {
+      // expires: new Date(Date.now() + data.expires_in),
+      secure: process.env.NODE_ENV === "production",
+      path: "/",
+    });
+    response.cookies.set("rft", data.refreshToken, {
+      // expires: new Date(Date.now() + data.refresh_token_expires_in),
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: process.env.NODE_ENV === "production",
+      path: "/",
+    });
+    return response;
   }
+  return NextResponse.json(res.body);
 }
