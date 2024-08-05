471b36f8cb4c641291d60e6479f6032014236661
rosevilleage <chanhee.frontend@gmail.com>
2024-08-05 12:46:46 +0900

    time fommating 함수 생성

--- src/utils/timestamp.ts
+++ src/utils/timestamp.ts
@@ -2,3 +2,8 @@ export const getTimestamp = () => {
   const timeNow = new Date();
   return Math.floor(timeNow.getTime() / 1000);
 };
+
+export const toFetchTimeString = (date: Date) => {
+  const time = new Date().toISOString().split("T");
+  return time[0] + " " + time[1].slice(0, 8);
+};
