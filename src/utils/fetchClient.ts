9e93440c2b88f29480a5e9583dafda5f7042fd1b
rosevilleage <chanhee.frontend@gmail.com>
2024-08-05 21:04:56 +0900

    custom fetch 생성

--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -4,6 +4,7 @@ import returnFetch, {
 } from "return-fetch";
 import Cookies from "js-cookie";
 import { redirectUri, scope } from "@/app/auth/page";
+import { getTimestamp } from "./timestamp";
 type JsonRequestInit = Omit<NonNullable<FetchArgs[1]>, "body"> & {
   body?: object;
 };
@@ -115,3 +116,116 @@ export const fetcher = async <T>(url: FetchArgs[0], init?: JsonRequestInit) => {
   }
   return res;
 };
+
+class Fetcher {
+  private clientSideBaseUrl: string;
+  private serverSideBaseUrl: string;
+
+  constructor() {
+    this.clientSideBaseUrl = process.env.NEXT_PUBLIC_BASEURL || "";
+    this.serverSideBaseUrl = "http://localhost:3000/api/";
+  }
+
+  private async requestClient<T>(
+    url: FetchArgs[0],
+    init?: JsonRequestInit
+  ): Promise<JsonResponse<ApiResponse<T>>> {
+    const fetch = returnFetchJson({
+      baseUrl: this.clientSideBaseUrl,
+      interceptors: {
+        async request(requestArgs) {
+          const accessToken = Cookies.get("act");
+          if (accessToken) {
+            requestArgs[1] = {
+              ...requestArgs[1],
+              headers: {
+                ...requestArgs[1]?.headers,
+                authorization: `Bearer ${accessToken}`,
+                "Content-Type": "application/json",
+              },
+            };
+          }
+          return requestArgs;
+        },
+      },
+    });
+    return fetch(url, init);
+  }
+  private async requestServer<T>(
+    url: FetchArgs[0],
+    init?: JsonRequestInit
+  ): Promise<JsonResponse<ApiResponse<T>>> {
+    const fetch = returnFetchJson({
+      baseUrl: this.serverSideBaseUrl,
+    });
+    return fetch(url, init);
+  }
+  private async fetcher<T>(url: FetchArgs[0], init?: JsonRequestInit) {
+    const res = await this.requestClient<T>(url, init);
+    const { code } = res.body;
+    if (code === 1007) {
+      const newResponse = await this.requestServer<T>("auth/refresh", {
+        method: "POST",
+        credentials: "same-origin",
+      });
+      if (newResponse.body.code === 0) {
+        const secondRes = await fetchClient<T>(url, init);
+        if (secondRes.body.code === 0) return secondRes;
+        return newResponse;
+      } else {
+        window.Kakao &&
+          window.Kakao.Auth.authorize({
+            redirectUri,
+            scope,
+          });
+      }
+    }
+    return res;
+  }
+
+  get<T>(
+    url: string,
+    query?: { [key: string | number]: string | number },
+    init?: JsonRequestInit
+  ) {
+    const params: { [key: string]: string } = {
+      timestamp: getTimestamp().toString(),
+    };
+    let leng = 0;
+    for (let key in query) {
+      params[key] = String(query[key]);
+      leng++;
+    }
+
+    const fullUrl = leng > 0 ? url + "?" + new URLSearchParams(params) : url;
+    return this.fetcher<T>(fullUrl, { ...init, method: "GET" });
+  }
+
+  post<T>(url: string, body: object, init?: JsonRequestInit) {
+    return this.fetcher<T>(url, {
+      ...init,
+      method: "POST",
+      headers: {
+        ...(init?.headers || {}),
+      },
+      body: { timestamp: getTimestamp(), ...body },
+    });
+  }
+
+  put<T>(url: string, body: any, init?: JsonRequestInit) {
+    return this.fetcher<T>(url, {
+      ...init,
+      method: "PUT",
+      headers: {
+        ...(init?.headers || {}),
+      },
+      body: { timestamp: getTimestamp(), ...body },
+    });
+  }
+
+  delete<T>(url: string, init?: JsonRequestInit) {
+    return this.fetcher<T>(url, { ...init, method: "DELETE" });
+  }
+}
+
+export const customFetch = new Fetcher();
