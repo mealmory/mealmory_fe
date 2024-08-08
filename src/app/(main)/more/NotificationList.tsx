890ac7bafc4cb460f6e41400d585c3735d2673a3
rosevilleage <chanhee.frontend@gmail.com>
2024-08-08 22:08:56 +0900

    공지사항 목록 연동

--- src/app/(main)/more/NotificationList.tsx
+++ src/app/(main)/more/NotificationList.tsx
@@ -1,49 +1,66 @@
 "use client";
 
 import Pagination from "@/components/main/Pagination";
-import { fetchClient } from "@/utils/fetchClient";
+import { customFetch } from "@/utils/fetchClient";
+import Link from "next/link";
 import { useEffect, useState } from "react";
-type Notice = { id: number; title: string; description: string; date: string };
+
+type Notice = { id: number; title: string; date: string };
+interface NoticeResponse {
+  flag: 0 | 1;
+  notice: Array<Notice>;
+}
 const VIEW_NOTICE_LENGTH = 5;
 
 const NotificationList = () => {
-  const [data, setData] = useState<Notice[] | undefined>();
+  const [noticeData, setNoticeData] = useState<Array<Notice>>([]);
+  const [newNotice, setNewNotice] = useState(false);
   const [page, setPage] = useState(1);
 
   useEffect(() => {
-    fetchClient<Notice[]>("dummy/notice/search", {
-      method: "GET",
-    }).then((data) => setData(data.body.data));
-  }, []);
+    customFetch
+      .get<NoticeResponse>("notice/search", { page: page })
+      .then((res) => {
+        if (res.body.code === 0) {
+          if (res.body.data.flag === 1) {
+            setNewNotice(true);
+          }
 
-  const noticeList = data?.slice(
-    page * VIEW_NOTICE_LENGTH - VIEW_NOTICE_LENGTH,
-    page * VIEW_NOTICE_LENGTH
-  );
+          setNoticeData(res.body.data.notice);
+        }
+      });
+  }, [page]);
 
-  const pageLength = data ? Math.ceil(data.length / VIEW_NOTICE_LENGTH) : 1;
+  const pageLength = noticeData
+    ? Math.ceil(noticeData.length / VIEW_NOTICE_LENGTH)
+    : 1;
 
   return (
     <div className="w-full flex flex-col items-center gap-2 py-3">
       <p className="text-xl sm:text-2xl mb-2">공지사항</p>
-      {!noticeList || noticeList.length < 1 ? (
+      {noticeData.length < 1 ? (
         <div className="w-full rounded-xl shadow-border p-3 sm:p-6 flex justify-center items-center">
           <p>공지사항이 없습니다.</p>
         </div>
       ) : (
-        noticeList.map(({ id, title, description, date }) => (
-          <div
+        noticeData.map(({ id, title, date }, idx) => (
+          <Link
+            href={`/notice/${id}`}
             key={id}
-            className="w-full flex items-center justify-between rounded-xl shadow-border p-3 sm:p-6 dark:bg-cusdarkbanana"
+            className={
+              "w-full flex items-center justify-between rounded-xl shadow-border p-3 sm:p-6 dark:bg-cusdarkbanana "
+            }
           >
-            <div>
+            <div className="flex items-center gap-2">
               <p className="text-lg sm:text-xl w-full point-value">{title}</p>
-              <p className="sm:text-sm text-xs block w-full max-w-[210px] text-ellipsis">
-                {description}
-              </p>
+              {newNotice && idx === 0 ? (
+                <div className="bg-cusorange text-white text-xs text-center rounded-[5px] pt-[2px] px-[1px]">
+                  <p className="w-[18px] h-[18px]">N</p>
+                </div>
+              ) : null}
             </div>
             <p className="sm:text-sm text-xs text-nowrap">{date}</p>
-          </div>
+          </Link>
         ))
       )}
       {pageLength ? (
