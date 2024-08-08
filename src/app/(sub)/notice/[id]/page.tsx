df7b672877a891c6b36a3904926082bb3eb1ca92
rosevilleage <chanhee.frontend@gmail.com>
2024-08-08 22:10:25 +0900

    공지사항 상세 연동

--- src/app/(sub)/notice/[id]/page.tsx
+++ src/app/(sub)/notice/[id]/page.tsx
@@ -1,20 +1,44 @@
-export default function NoticeDetail({ params }: { params: { id: string } }) {
+import { getTimestamp } from "@/utils/timestamp";
+
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function NoticeDetail({
+  params,
+}: {
+  params: { id: string };
+}) {
   const { id } = params;
-  const data = {
-    title: "4월 공지사항",
-    date: "2024-04-05",
-    description:
-      "adsffasdfsadfadsfhgjavrewouiyfrbeavbaeuiybvaeirubaehbkvuayerbvafliuarbefilaubfilawifuhwaikfb",
-  };
+  const noticeData = await fetch(
+    `${process.env.NEXT_PUBLIC_BASEURL}notice/info?${new URLSearchParams({
+      timestamp: getTimestamp().toString(),
+      id: id,
+    })}`,
+    {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${cookies().get("act")?.value}`,
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((body) => {
+      if (body.code === 0) return body.data[0];
+      throw new Error("존재하지 않는 공지사항");
+    })
+    .catch((err) => {
+      return redirect("/more");
+    });
+
   return (
     <main className="w-full h-full ">
       <div className="w-full h-full min-h-[calc(100vh-55px)] sm:rounded-xl shadow-border overflow-hidden">
         <div className="border-b bg-cusbanana p-5">
-          <h1 className=" sm:text-2xl">{data.title}</h1>
-          <p>{data.date}</p>
+          <h1 className=" sm:text-2xl">{noticeData.title}</h1>
+          <p>{noticeData.date}</p>
         </div>
         <div className="w-full h-full p-3">
-          <p className=" break-words">{data.description}</p>
+          <p className=" break-words">{noticeData.description}</p>
         </div>
       </div>
     </main>
