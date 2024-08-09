70c99c717ef0d347f0426921463ee529150bf17e
rosevilleage <chanhee.frontend@gmail.com>
2024-08-09 22:05:46 +0900

    notice add modal page 생성

---
+++ src/app/(sub)/notice/add/page.tsx
@@ -0,0 +1,10 @@
+import NoticeAddForm from "@/components/modal/notice/NoticeAddForm";
+import LikeModalView from "../../LikeModalView";
+
+export default function NoticeAddPage() {
+  return (
+    <LikeModalView>
+      <NoticeAddForm />
+    </LikeModalView>
+  );
+}
---
+++ src/app/@modal/(.)notice/add/page.tsx
@@ -0,0 +1,10 @@
+import Modal from "@/components/modal/Modal";
+import NoticeAddForm from "@/components/modal/notice/NoticeAddForm";
+
+export default function NoticeAdd() {
+  return (
+    <Modal>
+      <NoticeAddForm />
+    </Modal>
+  );
+}
