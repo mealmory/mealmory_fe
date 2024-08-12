c38276bccab37a6620a496aeb3e7b1dac84f0245
rosevilleage <chanhee.frontend@gmail.com>
2024-08-12 21:08:32 +0900

    date locale 설정

--- src/utils/timestamp.ts
+++ src/utils/timestamp.ts
@@ -4,6 +4,18 @@ export const getTimestamp = () => {
 };
 
 export const toFetchTimeString = (date: Date) => {
-  const time = date.toISOString().split("T");
-  return time[0] + " " + time[1].slice(0, 8);
+  const yearMonthDay = date
+    .toLocaleDateString("ko-KR", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    })
+    .replaceAll(". ", "-");
+  const time = date.toLocaleTimeString("ko-KR", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  });
+  return `${yearMonthDay.substring(0, yearMonthDay.length - 1)} ${time}`;
 };
