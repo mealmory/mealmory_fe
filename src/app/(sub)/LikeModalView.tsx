766c2067018c061d535bfcf3efc459b62d9b25c8
rosevilleage <chanhee.frontend@gmail.com>
2024-08-13 00:26:31 +0900

    maxWidth props 추가

--- src/app/(sub)/LikeModalView.tsx
+++ src/app/(sub)/LikeModalView.tsx
@@ -3,16 +3,19 @@ import { ReactNode } from "react";
 const LikeModalView = ({
   children,
   maxContent,
+  maxWidth,
 }: {
   children: ReactNode;
   maxContent?: boolean;
+  maxWidth?: boolean;
 }) => {
   return (
     <div className="bg-black bg-opacity-35 fixed left-0 top-0 w-screen h-screen flex justify-center items-center">
       <div
         className={
-          "w-full h-full md:w-[360px] overflow-hidden bg-white dark:bg-cusdark sm:rounded-2xl " +
-          (maxContent ? "sm:max-h-[680px] sm:h-max" : "sm:h-[680px]")
+          "w-full h-full overflow-hidden bg-white dark:bg-cusdark sm:rounded-2xl " +
+          (maxContent ? "sm:max-h-[680px] sm:h-max " : "sm:h-[680px] ") +
+          (maxWidth ? "md:w-max" : "md:w-[360px]")
         }
       >
         {children}
