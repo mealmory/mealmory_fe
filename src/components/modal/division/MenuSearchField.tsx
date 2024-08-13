fc94832d5c2578527a309fe51dd0147061a01102
rosevilleage <chanhee.frontend@gmail.com>
2024-08-14 01:00:13 +0900

    메뉴 검색 연동

--- src/components/modal/division/MenuSearchField.tsx
+++ src/components/modal/division/MenuSearchField.tsx
@@ -2,10 +2,11 @@
 
 import useMealPlanStore from "@/store/mealPlanStore";
 import useMenuRegion from "@/store/menuRegionStore";
-import { fetcher } from "@/utils/fetchClient";
-import { getTimestamp } from "@/utils/timestamp";
+import { errorAlert } from "@/utils/alertFns";
+import { customFetch } from "@/utils/fetchClient";
+import { checkSpecialCharacters } from "@/utils/inputFns";
 import { BsSearch } from "@react-icons/all-files/bs/BsSearch";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 interface SearchResultDTO {
   amount: number;
@@ -26,20 +27,7 @@ interface SearchResultDTO {
   unit: "g" | "ml";
   value: number;
 }
-interface TempResult {
-  menu: string;
-  kcal: number;
-  amount: number;
-  unit: number;
-  did: number;
-  cid: number;
-  fid: number;
-  menu_spec: {
-    carbs: number;
-    protein: number;
-    fat: number;
-  };
-}
+
 const MenuSearchField = ({
   handleClose,
   handleSelfClick,
@@ -50,42 +38,49 @@ const MenuSearchField = ({
   const { menuRegion } = useMenuRegion();
   const [searchValue, setSearchValue] = useState("");
   const [searchResult, setSearchResult] = useState<SearchResultDTO[]>();
-  useEffect(() => {
-    fetcher<{
-      in: SearchResultDTO[];
-      out: SearchResultDTO[];
-      processed: SearchResultDTO[];
-    }>(
-      "main/data?" +
-        new URLSearchParams({ timestamp: getTimestamp().toString() }),
-      {
-        method: "GET",
-      }
-    ).then((res) => {
-      if (res.body.code === 0) {
-        const tempKey =
-          menuRegion.division === 1
-            ? "processed"
-            : menuRegion.division === 2
-            ? "in"
-            : "out";
-        setSearchResult(res.body.data[tempKey]);
-      }
-    });
-  }, []);
+  useEffect(() => {}, []);
+  function handleSearchClick(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (checkSpecialCharacters(searchValue) || searchValue.length === 0) {
+      const errorTitle =
+        searchValue.length === 0
+          ? "메뉴 이름을 입력해 주세요."
+          : "올바르지 않은 입력입니다.";
+      errorAlert(errorTitle, "", () => {
+        setSearchValue("");
+      });
+    } else {
+      customFetch
+        .get<SearchResultDTO[]>("meal/food", {
+          did: menuRegion.division,
+          cid: menuRegion.category,
+          name: searchValue,
+        })
+        .then((res) => {
+          const ok = res.body.code === 0;
+          setSearchResult(ok ? res.body.data : []);
+        });
+    }
+  }
   return (
-    <div className="w-[97%] h-[95%] mx-auto rounded-xl shadow-border overflow-x-hidden overflow-y-scroll pb-3">
-      <div className="w-full border-b flex items-center p-3">
+    <div className="w-[97%] h-[95%] pb-1 mx-auto rounded-xl shadow-border overflow-hidden">
+      <form
+        className="w-full border-b flex items-center p-3"
+        onSubmit={handleSearchClick}
+      >
         <input
-          className="flex-1 dark:bg-cusdark"
+          className="flex-1 dark:bg-cusdark outline-none "
+          name="search"
           type="text"
           placeholder="검색할 메뉴 이름을 입력해주세요."
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
         />
-        <BsSearch size={20} />
-      </div>
-      <div className="w-full px-3 pb-2 bg-gray-50 rounded-b-2xl">
+        <button type="submit">
+          <BsSearch size={20} />
+        </button>
+      </form>
+      <div className="w-full h-[calc(100%-49px)] px-2 bg-gray-50 rounded-b-2xl overflow-y-hidden">
         <button
           className="w-full border-b border-black p-2 py-3 flex items-center justify-between rounded-none"
           onClick={handleSelfClick}
@@ -96,14 +91,16 @@ const MenuSearchField = ({
           </small>
         </button>
         {/* 검색 결과 */}
-        {searchResult &&
-          searchResult.map((item) => (
-            <ResultItem
-              key={item.id}
-              foodData={item}
-              handleClose={handleClose}
-            />
-          ))}
+        <div className="w-full h-[calc(100%-49px)] pb-2 px-1 overflow-y-scroll overflow-x-hidden scroll-visible ">
+          {searchResult &&
+            searchResult.map((item) => (
+              <ResultItem
+                key={item.id}
+                foodData={item}
+                handleClose={handleClose}
+              />
+            ))}
+        </div>
       </div>
     </div>
   );
@@ -174,11 +171,11 @@ const ResultItem = ({
   }
   return (
     <button
-      className="w-full p-2 py-3 flex items-center justify-between rounded-lg bg-white dark:bg-cusdark shadow-border mt-2"
+      className="w-full p-2 py-3 flex items-center flex-nowrap justify-between gap-5 rounded-lg bg-white dark:bg-cusdark shadow-border mt-2 overflow-hidden"
       onClick={handleClick}
     >
-      <p>{foodData.name}</p>
-      <p className="text-xs">
+      <p className="w-max text-left text-nowrap">{foodData.name}</p>
+      <p className="flex-1 text-xs text-right overflow-hidden text-ellipsis text-nowrap">
         {foodData.make === "해당없음" ? "" : foodData.make}
       </p>
     </button>
