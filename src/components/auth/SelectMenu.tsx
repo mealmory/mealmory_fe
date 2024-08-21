f41d2f155edf1868292e9470f29f2f10e7dbaaf3
rosevilleage <chanhee.frontend@gmail.com>
2024-08-21 23:41:40 +0900

    react-select => selector 변경

--- src/components/auth/SelectMenu.tsx
+++ src/components/auth/SelectMenu.tsx
@@ -1,19 +1,13 @@
 "use client";
 
-import dynamic from "next/dynamic";
-import { Dispatch, SetStateAction } from "react";
-
-const Select = dynamic(() => import("react-select"), {
-  ssr: false,
-  loading: () => <div className="rounded-xl w-full pt-[13.3%] border" />,
-});
+import Selector from "../Selector";
 
 interface SelectMenuProps {
   label: string;
   options: Array<{ value: number; label: string }>;
   disabled?: boolean;
   value: number;
-  handleChange: (e: unknown) => void;
+  handleClick: (optionValue: number) => void;
 }
 
 const SelectMenu = ({
@@ -21,50 +15,21 @@ const SelectMenu = ({
   label,
   value,
   disabled,
-  handleChange,
+  handleClick,
 }: SelectMenuProps) => {
   return (
     <div>
       <p>{label}</p>
-      <Select
-        options={options}
-        value={options.filter((option) => option.value === value)}
-        isDisabled={disabled}
-        onChange={handleChange}
-        styles={{
-          control: (styles) => ({
-            ...styles,
-            borderRadius: 13,
-            padding: 5,
-            "&:focus": {
-              outline: "none",
-              border: "none",
-            },
-          }),
-          option: (styles, { isDisabled, isFocused, isSelected }) => {
-            return {
-              ...styles,
-              backgroundColor: isDisabled
-                ? undefined
-                : isSelected
-                ? "#FFFBD3"
-                : isFocused
-                ? "white"
-                : isDisabled
-                ? "#ccc"
-                : undefined,
-              color: isDisabled ? "#939393" : isSelected ? "#FFB800" : "black",
-              cursor: isDisabled ? "not-allowed" : "default",
-              boxShadow: "none",
-              border: 0,
-            };
-          },
-          input: (styles) => ({
-            ...styles,
-            outline: "none",
-            border: "none",
-          }),
-        }}
+      <Selector
+        className="p-[5px] rounded-xl focus:outline-none shadow-border"
+        titleClass="py-1 px-2"
+        disabled={disabled}
+        options={options.map(({ value, label }) => ({
+          name: label,
+          optionValue: value,
+        }))}
+        value={value}
+        handleClick={(value) => handleClick(value)}
       />
     </div>
   );
