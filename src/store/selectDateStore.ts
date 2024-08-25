055a4a302ec0e25704b3d15b65768ed8d93a451e
rosevilleage <chanhee.frontend@gmail.com>
2024-08-25 20:38:29 +0900

    useDate store period state 추가

--- src/store/selectDateStore.ts
+++ src/store/selectDateStore.ts
@@ -1,15 +1,21 @@
 import { create } from "zustand";
 
+export type Period = "day" | "week" | "month";
+
 interface UseDate {
   selectedDate: Date;
+  period: Period;
   init: () => void;
   changeDate: (newDate: Date) => void;
+  changePeriod: (newPeriod: Period) => void;
 }
 
 const useDate = create<UseDate>((set) => ({
   selectedDate: new Date(),
-  init: () => set({ selectedDate: new Date() }),
+  period: "day",
+  init: () => set({ selectedDate: new Date(), period: "day" }),
   changeDate: (newDate) => set({ selectedDate: newDate }),
+  changePeriod: (newPeriod) => set({ period: newPeriod }),
 }));
 
 export default useDate;
