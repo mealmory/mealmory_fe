d33bb399616af8fe952ecf13632bb636975e0fa5
rosevilleage <chanhee.frontend@gmail.com>
2024-08-26 01:48:24 +0900

    time state 추가

--- src/store/selectDateStore.ts
+++ src/store/selectDateStore.ts
@@ -7,15 +7,60 @@ interface UseDate {
   period: Period;
   init: () => void;
   changeDate: (newDate: Date) => void;
+  changeTime: (hour: number, minute: number) => void;
+  changeFullDate: (fullDate: Date) => void;
   changePeriod: (newPeriod: Period) => void;
+  time: [number, number];
 }
 
-const useDate = create<UseDate>((set) => ({
-  selectedDate: new Date(),
-  period: "day",
-  init: () => set({ selectedDate: new Date(), period: "day" }),
-  changeDate: (newDate) => set({ selectedDate: newDate }),
-  changePeriod: (newPeriod) => set({ period: newPeriod }),
-}));
+const useDate = create<UseDate>((set) => {
+  const now = new Date();
+  return {
+    selectedDate: now,
+    period: "day",
+    time: [now.getHours(), now.getMinutes()],
+    init: () => {
+      const toDay = new Date();
+      set({
+        selectedDate: toDay,
+        period: "day",
+        time: [toDay.getHours(), toDay.getMinutes()],
+      });
+    },
+
+    changeFullDate: (fullDate) =>
+      set({
+        selectedDate: fullDate,
+        time: [fullDate.getHours(), fullDate.getMinutes()],
+      }),
+
+    changeDate: (newDate) => {
+      set((state) => {
+        const changedDate = new Date(
+          newDate.getFullYear(),
+          newDate.getMonth(),
+          newDate.getDate(),
+          state.time[0],
+          state.time[1]
+        );
+        return { selectedDate: changedDate };
+      });
+    },
+    changeTime: (hour, minute) =>
+      set((state) => {
+        const { selectedDate } = state;
+        const changedDate = new Date(
+          selectedDate.getFullYear(),
+          selectedDate.getMonth(),
+          selectedDate.getDate(),
+          hour,
+          minute
+        );
+
+        return { selectedDate: changedDate, time: [hour, minute] };
+      }),
+    changePeriod: (newPeriod) => set({ period: newPeriod }),
+  };
+});
 
 export default useDate;
