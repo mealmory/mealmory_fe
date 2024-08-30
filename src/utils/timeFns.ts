4b8e84a1a318eb526214b40a2d3abc61f436dc39
rosevilleage <chanhee.frontend@gmail.com>
2024-08-30 23:24:19 +0900

    util fns 추가

--- src/utils/timeFns.ts
+++ src/utils/timeFns.ts
@@ -1,3 +1,5 @@
+import { Period } from "@/store/selectDateStore";
+
 export const getTimestamp = () => {
   const timeNow = new Date();
   return Math.floor(timeNow.getTime() / 1000);
@@ -28,4 +30,40 @@ export const toMMddString = (date: Date) => {
     })
     .replaceAll(". ", "-");
   return MMdd.substring(0, MMdd.length - 1);
+};
+
+export const toyyMMddString = (date: Date) => {
+  const MMdd = date
+    .toLocaleDateString("ko-KR", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    })
+    .replaceAll(". ", "-");
+  return MMdd.substring(0, MMdd.length - 1);
+};
+
+export const genStartEndDate = (date: Date, period: Period) => {
+  if (period === "day") return undefined;
+  if (period === "week") {
+    const dayOfWeek = date.getDay();
+
+    const startDate = new Date(date);
+    startDate.setDate(date.getDate() - dayOfWeek);
+
+    const endDate = new Date(startDate);
+    endDate.setDate(startDate.getDate() + 6);
+
+    return {
+      startDate,
+      endDate,
+    };
+  }
+  const startDate = new Date(date.getFullYear(), date.getMonth(), 1);
+  const endDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+
+  return {
+    startDate,
+    endDate,
+  };
 };
