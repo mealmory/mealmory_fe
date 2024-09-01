d7bf24711270ab9ad04ea0e3d0603cb69975101a
rosevilleage <chanhee.frontend@gmail.com>
2024-09-01 23:27:41 +0900

    로그아웃 버튼 생성

--- src/app/(main)/_components/Navigator.tsx
+++ src/app/(main)/_components/Navigator.tsx
@@ -1,6 +1,6 @@
 import { LINK_ITEMS } from "@/constants/navConstants";
 import { IconType } from "@react-icons/all-files";
-import { HiDownload } from "@react-icons/all-files/hi/HiDownload";
+import { HiX } from "@react-icons/all-files/hi/HiX";
 import Link from "next/link";
 import { CSSProperties } from "react";
 
@@ -12,6 +12,7 @@ interface NavigatorProps {
   iconSize?: number | string;
   navFlip: () => void;
   navTransition: CSSProperties;
+  handleLogoutClick: () => void;
 }
 
 export default function Navigator({
@@ -22,24 +23,33 @@ export default function Navigator({
   iconSize,
   navFlip,
   navTransition,
+  handleLogoutClick,
 }: NavigatorProps) {
   return (
     <nav className={navClass} style={navTransition}>
       <div className={middleClass}>
-        <button className="self-end sm:hidden" onClick={navFlip}>
-          <HiDownload size={30} className=" -rotate-90" />
+        <div className="flex flex-col  gap-6 sm:gap-4">
+          <button className="self-end sm:hidden" onClick={navFlip}>
+            <HiX size={30} />
+          </button>
+          {LINK_ITEMS.map(({ title, link, icon }) => (
+            <NavItem
+              key={title}
+              title={title}
+              link={link}
+              active={pathname === link}
+              className={itemClass}
+              icon={icon}
+              iconSize={iconSize}
+            />
+          ))}
+        </div>
+        <button
+          className="w-full p-2 text-gray-400"
+          onClick={handleLogoutClick}
+        >
+          로그아웃
         </button>
-        {LINK_ITEMS.map(({ title, link, icon }) => (
-          <NavItem
-            key={title}
-            title={title}
-            link={link}
-            active={pathname === link}
-            className={itemClass}
-            icon={icon}
-            iconSize={iconSize}
-          />
-        ))}
       </div>
     </nav>
   );
