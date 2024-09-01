84c780905a34ef7846b537ab9089580a8e8fe0fb
rosevilleage <chanhee.frontend@gmail.com>
2024-09-01 22:31:12 +0900

    rewrites 적용

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ import createMDX from "@next/mdx";
 const nextConfig = {
   reactStrictMode: false,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  async rewrites() {
+    return [
+      {
+        source: "/api/data/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BASEURL}:path*`,
+      },
+    ];
+  },
 };
 
 const withMDX = createMDX({
