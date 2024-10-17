910986aec0a2e32a88226482591c43db6a68ff83
rosevilleage <chanhee.frontend@gmail.com>
2024-10-17 18:38:14 +0900

    logout 실패 response

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -5,6 +5,16 @@ export async function POST(req: NextRequest) {
   cookies().delete("act");
   cookies().delete("rft");
 
+  const actCookie = cookies().get("act")?.value;
+  const rftCookie = cookies().get("rft")?.value;
+
+  if (actCookie || rftCookie) {
+    return NextResponse.json({
+      result: "N",
+      code: 1,
+      message: "Failed",
+    });
+  }
   return NextResponse.json({
     result: "Y",
     code: 0,
