d69feacf861b57dd93c3a3dd9091256e05b145b7
rosevilleage <chanhee.frontend@gmail.com>
2024-11-01 13:43:55 +0900

    서비스 점검 고지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,18 @@ import { useEffect } from "react";
 import Cookies from "js-cookie";
 
 export default function Root() {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    if (Cookies.get("act")) {
-      router.replace("/home");
-    } else {
-      router.replace("/auth");
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (Cookies.get("act")) {
+  //     router.replace("/home");
+  //   } else {
+  //     router.replace("/auth");
+  //   }
+  // }, []);
 
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center gap-2">
+    <div className="w-full h-screen flex flex-col justify-center items-center gap-2">
       <Image
         src="/mealmory_logo.svg"
         alt="밀모리 로고"
@@ -25,8 +25,13 @@ export default function Root() {
         height={100}
         className=" mb-4"
       />
-      <h1 className=" text-4xl">MealMory</h1>
+      <h1 className=" text-4xl point-value">MealMory</h1>
       <h2 className=" text-xl">식사의 추억</h2>
+      <p className="text-center mt-6">
+        현재 서비스를 점검중 입니다.
+        <br />
+        이용에 불편을 드려 죄송합니다.
+      </p>
     </div>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
-  if (pathname === "/") {
-    if (req.cookies.get("act")) {
-      return NextResponse.redirect(new URL("/home", req.url));
-    } else {
-      return NextResponse.redirect(new URL("/auth", req.url));
-    }
-  } else {
-    return NextResponse.next();
-  }
+  // if (pathname === "/") {
+  //   if (req.cookies.get("act")) {
+  //     return NextResponse.redirect(new URL("/home", req.url));
+  //   } else {
+  //     return NextResponse.redirect(new URL("/auth", req.url));
+  //   }
+  // } else {
+  //   return NextResponse.next();
+  // }
+  return NextResponse.next();
 }
