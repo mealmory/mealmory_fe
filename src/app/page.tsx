4c774546ed4e061bf933576ee32976c89ecf1ec0
rosevilleage <chanhee.frontend@gmail.com>
2024-11-21 13:32:32 +0900

    점검 고지 원복

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,15 @@ import { useEffect } from "react";
 import Cookies from "js-cookie";
 
 export default function Root() {
-  // const router = useRouter();
+  const router = useRouter();
 
-  // useEffect(() => {
-  //   if (Cookies.get("act")) {
-  //     router.replace("/home");
-  //   } else {
-  //     router.replace("/auth");
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (Cookies.get("act")) {
+      router.replace("/home");
+    } else {
+      router.replace("/auth");
+    }
+  }, []);
 
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center gap-2">
@@ -27,11 +27,6 @@ export default function Root() {
       />
       <h1 className=" text-4xl point-value">MealMory</h1>
       <h2 className=" text-xl">식사의 추억</h2>
-      <p className="text-center mt-6">
-        현재 서비스를 점검중 입니다.
-        <br />
-        이용에 불편을 드려 죄송합니다.
-      </p>
     </div>
   );
 }
